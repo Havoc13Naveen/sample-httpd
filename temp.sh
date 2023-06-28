26e8d4a53ae1fe42212327e3978cacb1942c6845
Ubuntu <ubuntu@ip-172-31-7-28.us-east-2.compute.internal>
2023-06-28 08:45:39 +0000

    deleted

--- temp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-tag=`docker images |grep -i "havoc13/httpd" | awk -F " " '{print $2}'`
-echo "the tag of docker images is" $tag
-tags=$(echo "$tag + 1" | bc)
-echo "new tag" $tags
-sudo docker build -t havoc13/httpd:$tags -f dock .
-sudo docker push havoc13/httpd:$tags
-sudo docker rmi havoc13/httpd:$tag
