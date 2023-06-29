4919e6dca020fb5a35da9016abb3b6053c4c614e
havoc13naveen <ubuntu@ip-172-31-7-28.us-east-2.compute.internal>
2023-06-29 05:00:52 +0000

    adding

--- gitpush.sh
+++ gitpush.sh
@@ -4,7 +4,7 @@ git commit -m "adding"
 git status
 
 
-gitusername=$("havoc13naveen")
-gittoken=$("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")
+gitusername=$(havoc13naveen)
+gittoken=$(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)
 
 git push --credentials username=$gitusername,token=$gittoken
